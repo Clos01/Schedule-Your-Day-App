aad415d475b8e24c25de582c04e9f4d1f08d47a1
Carlos Rivas <Cjrivas00@gmail.com>
2022-02-04 21:06:06 -0500

    added current day in js and set variables for hours

---
+++ Develop/js/script.js
@@ -0,0 +1,16 @@
+// get current day using SVGAnimateMotionElement.js
+let currentDay = moment().format("dddd, MMM Do");
+console.log(currentDay)
+
+// Set each timeblock in the daily schedule using moment.js
+let hour9 = moment().hour(9);
+let hour10 = moment().hour(10);
+let hour11 = moment().hour(11);
+let hour12 = moment().hour(12);
+let hour13 = moment().hour(13);
+let hour14 = moment().hour(14);
+let hour15 = moment().hour(15);
+let hour16 = moment().hour(16);
+let hour17 = moment().hour(17);
+let hour18 = moment().hour(18);
+
