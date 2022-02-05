dbfa9091703bea198d27dcf91dfce9c0189b83cb
Carlos Rivas <Cjrivas00@gmail.com>
2022-02-04 21:18:21 -0500

    setting up local data storage

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,6 +1,6 @@
 // get current day using SVGAnimateMotionElement.js
 let currentDay = moment().format("dddd, MMM Do");
-console.log(currentDay)
+// console.log(currentDay)
 
 // Set each timeblock in the daily schedule using moment.js
 let hour9 = moment().hour(9);
@@ -14,3 +14,16 @@ let hour16 = moment().hour(16);
 let hour17 = moment().hour(17);
 let hour18 = moment().hour(18);
 
+// Creatting an array of hour blocks for code generation
+let hours = [hour9, hour10. hour11, hour12, hour13, hour14, hour15, hour16, hour16, hour17, hour18,];
+
+// Getting Local data Storage or setting to empty 
+let events9  = JSON.parse(localStorage.getItem('hour9')) || "";
+let events10 = JSON.parse(localStorage.getItem('hour10')) || "";
+let events11 = JSON.parse(localStorage.getItem('hour11')) || "";
+let events12 = JSON.parse(localStorage.getItem('hour12')) || "";
+let events13 = JSON.parse(localStorage.getItem('hour13')) || "";
+let events14 = JSON.parse(localStorage.getItem('hour14')) || "";
+let events15 = JSON.parse(localStorage.getItem('hour15')) || "";
+let events16 = JSON.parse(localStorage.getItem('hour16')) || "";
+let events17 = JSON.parse(localStorage.getItem('hour17')) || "";
