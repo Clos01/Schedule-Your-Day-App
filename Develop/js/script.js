b0ed4e6c24e624ceecf6fa399577036aeb09615c
Carlos Rivas <Cjrivas00@gmail.com>
2022-02-05 10:19:36 -0500

    updatded and finished style css debugged my code for js because 12 and one werent working properly

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -56,7 +56,7 @@ let auditTime = function (){
     if (moment().isBetween(hour10, hour11)){
         $("#timeblock10").addClass("present");
     }
-    else if (moment().isAfter(hour10)){
+    else if (moment().isAfter(hour11)){
         $("#timeblock10").addClass("past");
     }
     else {
@@ -66,37 +66,39 @@ let auditTime = function (){
     if (moment().isBetween(hour11, hour12)){
         $("#timeblock11").addClass("present");
     }
-    else if (moment().isAfter(hour11)){
+    else if (moment().isAfter(hour12)){
         $("#timeblock11").addClass("past");
     }
     else {
         $("#timeblock11").addClass("future");
     }
     //Audit hour 12
-    if (moment().isBetween(hour12, hour13)){
+    if (moment().isBetween(hour12, hour13)) {
         $("#timeblock12").addClass("present");
     }
-    else if (moment().isAfter(hour12)){
-        $("#timeblock12").addClass(past);
+    else if (moment().isAfter(hour13)) {
+        $("#timeblock12").addClass("past");
     }
     else {
-        $("timeblock12").addClass("future");
+        $("#timeblock12").addClass("future");
     }
+
     //Audit hour 13
-    if (moment().isBetween(hour13, hour14)){
+    if (moment().isBetween(hour13, hour14)) {
         $("#timeblock13").addClass("present");
     }
-    else if (moment().isAfter(hour13)){
-        $("timeblock13").addClass("past");
+    else if (moment().isAfter(hour14)) {
+        $("#timeblock13").addClass("past");
     }
     else {
         $("#timeblock13").addClass("future");
     }
+
     //Audit hour 14
     if (moment().isBetween(hour14, hour15)){
         $("timeblock14").addClass("present");
     }
-    else if (moment().isAfter(hour14)){
+    else if (moment().isAfter(hour15)){
         $("#timeblock14").addClass("past");
     }
     else{
@@ -106,7 +108,7 @@ let auditTime = function (){
     if (moment().isBetween(hour15, hour16)){
         $("#timeblock15").addClass("present");
     }
-    else if (moment().isAfter(hour15)){
+    else if (moment().isAfter(hour16)){
         $("#timeblock15").addClass("past");
     }
     else {
@@ -116,7 +118,7 @@ let auditTime = function (){
     if (moment().isBetween(hour16, hour17)){
         $("#timeblock16").addClass("present");
     }
-    else if (moment().isAfter(hour15)){
+    else if (moment().isAfter(hour17)){
         $("#timeblock16").addClass("past");
     }
     else{
@@ -125,7 +127,7 @@ let auditTime = function (){
     if (moment().isBetween(hour16, hour17)){
         $("#timeblock16").addClass("present");
     }
-    else if (moment().isAfter(hour16)){
+    else if (moment().isAfter(hour18)){
         $("#timeblock16").addClass("past");
     }
     else {
